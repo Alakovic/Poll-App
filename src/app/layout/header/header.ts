94ac516900bc3ece481906a8cfd460412e3dea67
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-03-26 22:52:14 +0100

    started with survey details

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass, RouterLink],
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
 })
 export class Header {
+  router = inject(Router);
+
+  isSurveyDetails(): boolean {
+    return this.router.url.startsWith('/survey/');
+  }
+
+  isHidden(): boolean {
+    return this.router.url === '/survey-form' || this.router.url === '/';
+  }
+
+  getLogo(): string {
+    if (this.isSurveyDetails()) {
+      return './assets/img/icon_purpledark.svg';
+    }
+    return './assets/img/icon_orange.svg';
+  }
+
 }
